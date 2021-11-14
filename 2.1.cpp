b663c3d353c66351b802f8d9589164259b9cbbcf
Dima Kachkovskii <90241790+MACTEPOB@users.noreply.github.com>
2021-11-15 01:29:42 +0500

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, sum;
+    sum = 0;
+    cout << "Введите число N" << endl;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        sum = sum + i;
+    }
+    cout << "Результат: " << sum;
+    return 0;
+} 
