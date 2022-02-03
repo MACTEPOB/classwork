566c15015154e850c68b332fd3276859147fa1fe
Dima Kachkovskii <90241790+MACTEPOB@users.noreply.github.com>
2022-02-03 13:26:22 +0500

    Update 3.3.cpp

--- 3.3.cpp
+++ 3.3.cpp
@@ -15,7 +15,7 @@ int main()
     cout << endl;
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j <= n - 1 - i; j++)
+        for (int j = 0; j <= n - 2 - i; j++)
         {
             if (a[j] > a[j + 1])
             {
